b9948fb36ef877749defa7bec6a0c1c6f33c5677
Michael Ramírez <michalramirez36@gmail.com>
2023-10-31 23:28:13 -0600

    :hammer: Keeping going

--- src/main.js
+++ src/main.js
@@ -6,3 +6,29 @@ const checkoutButton = document.getElementById('checkout-button');
 const clearButton = document.getElementById('clear-button');
 const orderHistory = document.getElementById('order-history');
 
+// Define una clase llamada "Product" para representar productos
+class Product {
+    constructor(id, name, price) {
+      this.id = id;
+      this.name = name;
+      this.price = price;
+    }
+
+    // Genera una tarjeta de producto en el DOM
+  generateProductCard() {
+    const card = document.createElement('div');
+    card.classList.add('bg-white', 'h-64', 'rounded', 'p-4', 'shadow', 'product-card');
+    card.innerHTML = `
+      <h2 class="text-xl font-bold mb-2">${this.name}</h2>
+      <p class="mb-2">Descripción del ${this.name}</p>
+      <p class="text-gray-500">$${this.price}</p>
+      <button class="add-button bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">Agregar al carrito</button>
+    `;
+
+    // Agrega un manejador de eventos al botón "Agregar al carrito"
+    const addButton = card.querySelector('add-button');
+    addButton.addEventListener('click', () => addToCart(this));
+
+    return card;
+  }
+}
